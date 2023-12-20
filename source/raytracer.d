b77dab62dc09fb4c6438079c3f09d6a6b8046a2e
HuskyNator <HuskyNator@protonmail.ch>
2023-12-20 18:55:18 +0100

    Add projected barycentric calculation

--- source/raytracer.d
+++ source/raytracer.d
@@ -1,7 +1,8 @@
 module raytracer;
-import vertexd.core;
+import bvh;
 import raycam;
 import screen;
+import vertexd.core;
 
 float u_occlusion = .1;
 float u_diffuse = .7;
@@ -25,7 +26,10 @@ struct Scene {
 		this.triangleNormals = [];
 		this.triangleNormals.reserve(indices.length);
 		foreach (uint[3] triangle; indices) {
-			Vec!3[3] pos = [positions[triangle[0]], positions[triangle[1]], positions[triangle[2]]];
+			Vec!3[3] pos = [
+				positions[triangle[0]], positions[triangle[1]],
+				positions[triangle[2]]
+			];
 			this.triangleNormals ~= (pos[1] - pos[0]).cross(pos[2] - pos[0]).normalize();
 		}
 	}
@@ -109,9 +113,11 @@ struct RayTracer {
 		Vec!3 point = ray.org + ray.dir * dist;
 		uint[3] triangle = scene.indices[index];
 		Vec!3[3] positions = [
-			scene.positions[triangle[0]], scene.positions[triangle[1]], scene.positions[triangle[2]]
+			scene.positions[triangle[0]], scene.positions[triangle[1]],
+			scene.positions[triangle[2]]
 		];
-		Vec!3 barycentric = calcBarycentric(positions, scene.triangleNormals[index], point);
+		// Vec!3 barycentric = calcBarycentric(positions, scene.triangleNormals[index], point);
+		Vec!3 barycentric = calcProjectedBarycentric(positions, point);
 		foreach (λ; barycentric)
 			if (λ < 0)
 				return -1;
@@ -131,25 +137,78 @@ struct RayTracer {
 		return distance;
 	}
 
-	Vec!3 calcBarycentric(Vec!3[3] verteces, Vec!3 normal, Vec!3 point) {
-		Vec!3 v0 = verteces[0];
-		Vec!3 v1 = verteces[1];
-		Vec!3 v2 = verteces[2];
-		float area0 = (v1 - point).cross(v2 - point).dot(normal);
-		float area1 = (v2 - point).cross(v0 - point).dot(normal);
-		float area2 = (v0 - point).cross(v1 - point).dot(normal);
-		float fullArea = (v1 - v0).cross(v2 - v0).dot(normal); // better precision
-		// float fullArea = area0 + area1 + area2;
+	Vec!3 calcBarycentric(Vec!3[3] vertexes, Vec!3 normal, Vec!3 point) {
+		Vec!3 pToV0 = vertexes[0] - point;
+		Vec!3 pToV1 = vertexes[1] - point;
+		Vec!3 pToV2 = vertexes[2] - point;
+		float area0 = (pToV1).cross(pToV2).dot(normal);
+		float area1 = (pToV2).cross(pToV0).dot(normal);
+		float area2 = (pToV0).cross(pToV1).dot(normal);
+		float fullArea = area0 + area1 + area2;
 
-		debug import std.format;
-
-		debug import std.math : abs;
-
-		debug float fullArea2 = area0 + area1 + area2;
-		// ???
+		// TODO
+		// float fullArea = (v1 - v0).cross(v2 - v0).dot(normal); // very much not equal??
 		// assert((fullArea - fullArea2).abs < 0.1, "Expected area " ~ format("%.8f",
 		// 		fullArea) ~ " but got " ~ format("%.8f", fullArea2));
 		return Vec!3(area0, area1, area2) / fullArea;
 	}
 
+	/// Alternative way projection onto 2d xyz planes.
+	Vec!3 calcProjectedBarycentric(Vec!3[3] verts, Vec!3 point) {
+		Vec!3 normal = (verts[1] - verts[0]).cross(verts[2] - verts[0]);
+		import std.math : abs;
+
+		// TODO Proof? source:https://ceng2.ktu.edu.tr/~cakir/files/grafikler/Texture_Mapping.pdf
+		float xArea = abs(normal.x); // = * 2
+		float yArea = abs(normal.y); // = * 2
+		float zArea = abs(normal.z); // = * 2
+
+		if (xArea >= yArea && xArea >= zArea) {
+			float xAreaFrac = 1.0f / xArea;
+			return calcProjectedBarycentric!("y", "z")(xArea, verts, point);
+		} else if (yArea >= zArea) {
+			return calcProjectedBarycentric!("x", "z")(yArea, verts, point);
+		} else {
+			return calcProjectedBarycentric!("x", "y")(zArea, verts, point);
+		}
+	}
+
+	private Vec!3 calcProjectedBarycentric(string firstAxis, string secondAxis)(
+		float fullArea, Vec!3[3] verts, Vec!3 point) {
+		pragma(inline);
+		float fullAreaFrac = 1.0f / fullArea;
+		Vec!3 bary;
+		bary[0] = triangleAreaDouble([
+			Vec!2(mixin("point." ~ firstAxis), mixin("point." ~ secondAxis)),
+			Vec!2(mixin("verts[1]." ~ firstAxis), mixin("verts[1]." ~ secondAxis)),
+			Vec!2(mixin("verts[2]." ~ firstAxis), mixin("verts[2]." ~ secondAxis))
+		]) * fullAreaFrac;
+		bary[1] = triangleAreaDouble([
+			Vec!2(mixin("point." ~ firstAxis), mixin("point." ~ secondAxis)),
+			Vec!2(mixin("verts[2]." ~ firstAxis), mixin("verts[2]." ~ secondAxis)),
+			Vec!2(mixin("verts[1]." ~ firstAxis), mixin("verts[1]." ~ secondAxis))
+		]) * fullAreaFrac;
+		bary[2] = 1.0f - bary[0] - bary[1];
+		return bary;
+	}
+
+	private float triangleAreaDouble(Vec!2[3] verts) {
+		pragma(inline);
+		return (verts[0].x - verts[1].x) * (verts[1].y - verts[2].y) + (
+			verts[1].x - verts[2].x) * (verts[1].y - verts[0].y);
+	}
+
 }
+
+// (x2-x1)*(y3-y1) - (x3-x1)*(y3-y1)/2 - (x2-x1)*(y2-y1)/2 - (x2-x3)*(y3-y2)/2
+// 		=
+// 		x1(y1-y3+y3/2-y1/2+y2/2-y1/2)
+// 		+x2(y3-y1-y2/2+y1/2-y3/2+y2/2)
+// 		+x3(-y3/2+y1/2+y3/2-y2/2)
+// 		=
+// 		x1(y2/2-y3/2)
+// 		+x2(-y1/2+y3/2)
+// 		+x3(y1/2-y2/2)
+// 		=
+// 		((x1-x2)*(y2-y3)
+// 		+(x2-x3)*(y2-y1))/2
