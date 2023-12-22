8895577b393d8ebcb1ae05e1f0fe472f86c79990
HuskyNator <HuskyNator@protonmail.ch>
2023-12-22 21:57:13 +0100

    Add Gif creation

--- source/app.d
+++ source/app.d
@@ -1,3 +1,4 @@
+import gamut;
 import player;
 import raycam;
 import raytracer;
@@ -5,23 +6,26 @@ import screen;
 import std.conv;
 import std.datetime.stopwatch;
 import std.file;
+import std.math.constants : PI;
+import std.path;
 import std.process;
 import std.stdio;
 import std.string;
 import vertexd.core;
-import std.path;
 import vertexd.input.gltf_reader;
 import vertexd.mesh;
 import vertexd.misc : degreesToRadians;
 import vertexd.world : World;
 
 void main(string[] args) {
-	uint width = 1920/3;
-	uint height = 1080/3;
+	uint width = 1920 / 3;
+	uint height = 1080 / 3;
 
 	const bool RENDER_IMAGE = (args.length > 1 && args[1] == "image");
 	const bool PROFILE = (args.length > 1 && args[1] == "profile");
 	const uint PROFILE_COUNT = (PROFILE && args.length > 2) ? args[2].to!uint : 8;
+	const bool GIF = (args.length > 1 && args[1] == "gif");
+	const uint GIF_COUNT = (args.length > 2) ? args[2].to!uint : 16;
 
 	vdInit();
 	if (RENDER_IMAGE || PROFILE)
@@ -88,62 +92,27 @@ void main(string[] args) {
 			append(performancePath, commitName ~ ':' ~ performance.to!string ~ '\n');
 			writeln("Performance: ", performance, " sec");
 		}
-	} else
+	} else if (GIF) {
+		Vec!(4, ubyte)[][] images;
+
+		float angle = 2 * PI / (GIF_COUNT);
+		Quat rotation = Quat.rotation(Vec!3(0, 1, 0), angle);
+		Mat!3 rotationM = rotation.toMat;
+		foreach (i; 0 .. GIF_COUNT) {
+			rayTracer.trace(scene, 1, useBVH);
+			images ~= screen.texture.pixels.dup;
+			speler.location = rotationM ^ speler.location;
+		}
+		Image image;
+		image.createLayeredViewFromData(cast(void*) images.ptr, cast(int) width, cast(int) height,
+			cast(int) 4, PixelType.rgba8, cast(int)(width * 4 * ubyte.sizeof),
+			cast(int)(width * height * Vec!(4, ubyte).sizeof));
+		image.saveToFile("helmet.gif");
+	} else {
 		while (!vdShouldClose()) {
 			rayTracer.trace(scene, 1, useBVH);
 			vdStep();
 			// writeln("FPS: " ~ vdFps().to!string);
 		}
-	// import std.random : uniform;
-	// import vertexd.misc;
-
-	// enum RUNS = 300;
-	// Vec!3[3][RUNS] verts;
-	// Vec!3[RUNS] normal;
-	// Vec!3[RUNS] bary;
-	// Vec!3[RUNS] point;
-	// foreach (k; 0 .. RUNS) {
-	// 	foreach (i; 0 .. 3)
-	// 		foreach (j; 0 .. 3)
-	// 			verts[k][i][j] = uniform(-5.0, 5.0);
-	// 	normal[k] = (verts[k][1] - verts[k][0]).cross(verts[k][2] - verts[k][0]).normalize();
-
-	// 	bary[k][0] = uniform!"[]"(0.0, 1.0);
-	// 	bary[k][1] = uniform!"[]"(0.0, 1.0);
-	// 	bary[k][2] = 1.0 - bary[k][0] - bary[k][1];
-	// 	assertAlmostEqual(bary[k].sum(), 1.0);
-
-	// 	point[k] = Vec!3(0);
-	// 	static foreach (i; 0 .. 3)
-	// 		point[k] += verts[k][i] * bary[k][i];
-	// }
-
-	// Vec!3[RUNS] calculatedBary;
-	// Vec!3[RUNS] calculatedProjectedBary;
-
-	// import std.datetime.stopwatch;
-
-	// StopWatch firstWatch = StopWatch(AutoStart.no);
-	// StopWatch secondWatch = StopWatch(AutoStart.no);
-
-	// firstWatch.start();
-	// foreach (k; 0 .. RUNS)
-	// 	calculatedBary[k] = RayTracer.calcBarycentric(verts[k], normal[k], point[k]);
-	// firstWatch.stop();
-
-	// secondWatch.start();
-	// foreach (k; 0 .. RUNS)
-	// 	calculatedProjectedBary[k] = RayTracer.calcProjectedBarycentric(verts[k], point[k]);
-	// secondWatch.stop();
-
-	// foreach (k; 0 .. RUNS) {
-	// 	calculatedBary[k].assertAlmostEq(bary[k]);
-	// 	calculatedProjectedBary[k].assertAlmostEq(bary[k]);
-	// }
-
-	// import std.stdio;
-	// import std.conv;
-
-	// writeln("First: " ~ std.conv.to!string(firstWatch.peek().total!"hnsecs"()));
-	// writeln("Second: " ~ std.conv.to!string(secondWatch.peek().total!"hnsecs"()));
+	}
 }
